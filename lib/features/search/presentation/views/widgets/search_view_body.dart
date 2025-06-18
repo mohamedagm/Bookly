3ae288b856d56dafdcd83793c19b0292c21ba4b9
mohamedagm <abogm44ud@gmail.com>
2025-06-18 03:00:45 +0200

    add bookListView to searchView + done the ui #

--- lib/features/search/presentation/views/widgets/search_view_body.dart
+++ lib/features/search/presentation/views/widgets/search_view_body.dart
@@ -1,4 +1,6 @@
+import 'package:bookly/core/utils/styles.dart';
 import 'package:bookly/features/search/presentation/views/widgets/custom_text_field.dart';
+import 'package:bookly/features/search/presentation/views/widgets/search_result_list_view.dart';
 import 'package:flutter/material.dart';
 
 class SearchViewBody extends StatelessWidget {
@@ -6,6 +8,28 @@ class SearchViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Column(children: [CustomTextField()]));
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: IconButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: Icon(Icons.close, size: 30, color: Colors.white),
+              ),
+            ),
+            CustomTextField(),
+            SizedBox(height: 16),
+            Text('Results :', style: Styles.themeStyle),
+            SizedBox(height: 16),
+            SearchResultListView(),
+          ],
+        ),
+      ),
+    );
   }
 }
