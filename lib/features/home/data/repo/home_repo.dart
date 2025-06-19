fc9dd4411a9a6c9255eb517cbd695615633dd76e
mohamedagm <abogm44ud@gmail.com>
2025-06-19 15:35:52 +0200

    add returnType to methods in repo home + use dartz (handle error using Either)

--- lib/features/home/data/repo/home_repo.dart
+++ lib/features/home/data/repo/home_repo.dart
@@ -1,4 +1,8 @@
+import 'package:bookly/core/errors/failures.dart';
+import 'package:bookly/features/home/data/models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo {
-  fetchHorizontalListViewBooks();
-  fetchBestSellerListViewBooks();
+  Future<Either<Failures, List<BookModel>>> fetchHorizontalListViewBooks();
+  Future<Either<Failures, List<BookModel>>> fetchBestSellerListViewBooks();
 }
