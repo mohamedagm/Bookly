c1a9ef4530ae9feccaf48db61ee272505b884d5f
mohamedagm <abogm44ud@gmail.com>
2025-06-20 23:17:30 +0200

    done state + cubit method of bestSellerBooks

--- lib/features/home/presentation/Manager/bestSellerBooks/best_seller_books_cubit.dart
+++ lib/features/home/presentation/Manager/bestSellerBooks/best_seller_books_cubit.dart
@@ -1,8 +1,23 @@
 import 'package:bloc/bloc.dart';
+import 'package:bookly/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly/features/home/data/repo/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'best_seller_books_state.dart';
 
 class BestSellerBooksCubit extends Cubit<BestSellerBooksState> {
-  BestSellerBooksCubit() : super(BestSellerBooksInitial());
+  BestSellerBooksCubit(this.homeRepo) : super(BestSellerBooksInitial());
+  final HomeRepo homeRepo;
+  fetchHorizontalListViewBooks() async {
+    emit(BestSellerBooksLoading());
+    var result = await homeRepo.fetchBestSellerListViewBooks();
+    result.fold(
+      (faliure) {
+        emit(BestSellerBooksFaliure(faliure.errMessage));
+      },
+      (books) {
+        emit(BestSellerBooksSuccess(books));
+      },
+    );
+  }
 }
--- lib/features/home/presentation/Manager/bestSellerBooks/best_seller_books_state.dart
+++ lib/features/home/presentation/Manager/bestSellerBooks/best_seller_books_state.dart
@@ -8,3 +8,17 @@ sealed class BestSellerBooksState extends Equatable {
 }
 
 final class BestSellerBooksInitial extends BestSellerBooksState {}
+
+final class BestSellerBooksLoading extends BestSellerBooksState {}
+
+final class BestSellerBooksSuccess extends BestSellerBooksState {
+  final List<BookModel> books;
+
+  const BestSellerBooksSuccess(this.books);
+}
+
+final class BestSellerBooksFaliure extends BestSellerBooksState {
+  final String errMessage;
+
+  const BestSellerBooksFaliure(this.errMessage);
+}
