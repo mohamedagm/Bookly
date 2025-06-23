4842b4a68fa85b5ff135353d837910df1f156425
mohamedagm <abogm44ud@gmail.com>
2025-06-23 23:49:12 +0200

    Using Cached_network_image instead of normal one

--- lib/features/home/presentation/views/widgets/book_image_card.dart
+++ lib/features/home/presentation/views/widgets/book_image_card.dart
@@ -1,21 +1,30 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class BookImageCard extends StatelessWidget {
   const BookImageCard({super.key, required this.imageLink});
   final String imageLink;
+
   @override
   Widget build(BuildContext context) {
     return AspectRatio(
       aspectRatio: 2.7 / 4,
       child: Padding(
         padding: const EdgeInsets.only(right: 8.0),
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16),
-            image: DecorationImage(
-              fit: BoxFit.fill,
-              image: NetworkImage(imageLink),
-            ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(16),
+          child: CachedNetworkImage(
+            imageUrl: imageLink,
+            fit: BoxFit.cover,
+            placeholder:
+                (context, url) => Container(
+                  color: Colors.grey.shade200,
+                  child: const Center(
+                    child: Icon(Icons.image, size: 40, color: Colors.grey),
+                  ),
+                ),
+            errorWidget:
+                (context, url, error) => const Icon(Icons.broken_image),
           ),
         ),
       ),
