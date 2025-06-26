b7d20af19db0bd8dc4e46e7c9ed9c5fee4225b2e
mohamedagm <abogm44ud@gmail.com>
2025-06-26 05:50:40 +0200

    done method of searchResult Cubit

--- lib/features/search/presentation/Manager/SearchResult/search_result_cubit.dart
+++ lib/features/search/presentation/Manager/SearchResult/search_result_cubit.dart
@@ -1,9 +1,25 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly/core/models/book_model/book_model.dart';
+import 'package:bookly/features/search/data/repo/search_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'search_result_state.dart';
 
 class SearchResultCubit extends Cubit<SearchResultState> {
-  SearchResultCubit() : super(SearchResultInitial());
+  SearchResultCubit(this.searchRepo, this.query) : super(SearchResultInitial());
+  final SearchRepo searchRepo;
+  final String query;
+  fetchResultBooksC() async {
+    emit(SearchResultLoading());
+
+    var result = await searchRepo.fetchResultBooks(query: query);
+    result.fold(
+      (failure) {
+        emit(SearchResultFailure(failure.errMessage));
+      },
+      (books) {
+        emit(SearchResultSuccess(books));
+      },
+    );
+  }
 }
